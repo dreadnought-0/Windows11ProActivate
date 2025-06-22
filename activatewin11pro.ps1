8f0e964c6333c93b860908970293ffe9ab5dda2d
Dreadnought <me@dreadnought.work>
2025-06-22 05:03:11 -0600

    Update activatewin11pro.ps1
    File structure change.

--- activatewin11pro.ps1
+++ activatewin11pro.ps1
@@ -1,21 +1,19 @@
-# PowerShell script to reset KMS settings and activate Windows
-
 Write-Host "Uninstalling current product key..." -ForegroundColor Cyan
-Start-Process -FilePath "cscript.exe" -ArgumentList "slmgr.vbs /upk" -NoNewWindow -Wait
+Start-Process -FilePath "cscript.exe" -ArgumentList "C:\Windows\System32\slmgr.vbs /upk" -NoNewWindow -Wait
 
 Write-Host "Clearing the product key from the registry..." -ForegroundColor Cyan
-Start-Process -FilePath "cscript.exe" -ArgumentList "slmgr.vbs /cpky" -NoNewWindow -Wait
+Start-Process -FilePath "cscript.exe" -ArgumentList "C:\Windows\System32\slmgr.vbs /cpky" -NoNewWindow -Wait
 
 Write-Host "Clearing any configured KMS server..." -ForegroundColor Cyan
-Start-Process -FilePath "cscript.exe" -ArgumentList "slmgr.vbs /ckms" -NoNewWindow -Wait
+Start-Process -FilePath "cscript.exe" -ArgumentList "C:\Windows\System32\slmgr.vbs /ckms" -NoNewWindow -Wait
 
 Write-Host "Installing new product key..." -ForegroundColor Cyan
-Start-Process -FilePath "cscript.exe" -ArgumentList "slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX" -NoNewWindow -Wait
+Start-Process -FilePath "cscript.exe" -ArgumentList "C:\Windows\System32\slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX" -NoNewWindow -Wait
 
 Write-Host "Setting the KMS server to kms8.msguides.com..." -ForegroundColor Cyan
-Start-Process -FilePath "cscript.exe" -ArgumentList "slmgr.vbs /skms kms8.msguides.com" -NoNewWindow -Wait
+Start-Process -FilePath "cscript.exe" -ArgumentList "C:\Windows\System32\slmgr.vbs /skms kms8.msguides.com" -NoNewWindow -Wait
 
 Write-Host "Activating Windows..." -ForegroundColor Cyan
-Start-Process -FilePath "cscript.exe" -ArgumentList "slmgr.vbs /ato" -NoNewWindow -Wait
+Start-Process -FilePath "cscript.exe" -ArgumentList "C:\Windows\System32\slmgr.vbs /ato" -NoNewWindow -Wait
 
 Write-Host "All commands completed." -ForegroundColor Green
